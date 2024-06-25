89158492842b6d7aa9422d5b8bf62372e1967246
j431miyo <j520431k@mails.cc.ehime-u.ac.jp>
2024-06-25 19:38:07 +0900

    Update a.php

--- a.php
+++ a.php
@@ -2,5 +2,8 @@
 <?php 
   foreach(file('/usr/share/dict/words') as $line){
     // strpos() 関数を使って、文字列 "xy" が含まれる単語を echo "$line"; で表示しなさい。
+    if (strpos(&line,'xy') !== false) {
+      echo "$line\n";
+    }
   } 
 ?>
